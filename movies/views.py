a6545c9834cde2b66f9746b40d6202a3af83bd09
devShantanu-Kadam <shantanukadam2000@gmail.com>
2023-07-23 23:35:04 +0530

    fixed a response

--- movies/views.py
+++ movies/views.py
@@ -80,7 +80,7 @@ class GetMoviesView(APIView):
                     review["movie_id"] = _movie.id
                     Review.objects.update_or_create(review_id = review_id,defaults=review)
 
-            return Response({"success: True"})
+            return Response({"success": True})
         except Exception as e:
             return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
